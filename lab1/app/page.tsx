2d7e9f01ea2ecd6bcb667a1ed17ad0c2eb72317a
DuyLong-procoder <wincute4321@gmail.com>
2025-12-02 20:55:39 +0700

    Add Lab 1

---
+++ lab1/app/page.tsx
@@ -0,0 +1,12 @@
+
+import MyComponent from "./components/MyComponent";
+import Clock from "./components/Clock";
+
+export default function Home() {
+  return (
+    <main>
+      <MyComponent />
+      <Clock />
+    </main>
+  );
+}
